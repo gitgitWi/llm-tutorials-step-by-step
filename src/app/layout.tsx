72e57ddf28fdf5aa86699d0cfe4a0d9e4f01e8c1
gitgitWi <gitgitwi@gmail.com>
2024-09-25 00:33:31 +0900

    feat(layout) add styles to root-layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,18 @@ import { cn } from '~/lib/utils';
 import './globals.css';
 
 export const metadata: Metadata = {
-	title: 'LLM Step-by-Step',
-	description: 'Tutorial for building LLM apps',
+  title: 'LLM Step-by-Step',
+  description: 'Tutorial for building LLM apps',
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
-	return (
-		<html lang="ko" className={cn(GeistMono.variable, 'antialiased')}>
-			<body className="bg-gray-100 p-4">{children}</body>
-		</html>
-	);
+  return (
+    <html lang="ko" className={cn(GeistMono.variable, 'antialiased')}>
+      <body className="p-4 bg-neutral-100">
+        <div className="flex flex-col max-w-2xl p-4 mx-auto bg-white border rounded-lg border-3 border-neutral-200">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
 }
