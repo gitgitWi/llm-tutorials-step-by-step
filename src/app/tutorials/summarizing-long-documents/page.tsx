5f656b1a862bed6bc0e46df06d895c4cbb4812d9
gitgitWi <gitgitwi@gmail.com>
2024-09-26 08:23:50 +0900

    feat(tokens/page) add components to get tokens of text

--- src/app/tutorials/summarizing-long-documents/page.tsx
+++ src/app/tutorials/summarizing-long-documents/page.tsx
@@ -1,8 +1,13 @@
 'use client';
 
 import { valibotResolver } from '@hookform/resolvers/valibot';
+import { default as fetcher } from 'ky';
+import { SquareArrowOutUpRightIcon } from 'lucide-react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import type { TiktokenModel } from 'tiktoken';
 import { type InferOutput, minLength, object, pipe, string } from 'valibot';
+import { useApiToken } from '~/features/api-token';
 import {
   Card,
   CardContent,
@@ -12,6 +17,13 @@ import {
 import { Form, FormControl, FormField, FormItem } from '~/features/ui/form';
 import { Heading2, Heading3 } from '~/features/ui/headings';
 import { Input } from '~/features/ui/input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '~/features/ui/select';
 import { Textarea } from '~/features/ui/textarea';
 
 // TODO: 스키마 정의
@@ -24,6 +36,8 @@ const SummarizeLongDocFormSchema = object({
     string(),
     minLength(2, '문서 텍스트는 최소 2자 이상이어야 합니다.')
   ),
+  // TODO enum TiktokenModel
+  modelName: pipe(string(), minLength(1)),
 });
 
 type SummarizeLongDocFormSchema = InferOutput<
@@ -31,22 +45,44 @@ type SummarizeLongDocFormSchema = InferOutput<
 >;
 
 export default function SummarizingLongDocumentsPage() {
+  /** @TODO form -> 전역상태로 저장 */
+  const { apiToken, setApiToken } = useApiToken();
+
+  // TODO: react-query 사용
+  const [encodedTokens, setEncodedTokens] = useState<Record<number, number>>(
+    {}
+  );
+
   const form = useForm<SummarizeLongDocFormSchema>({
     resolver: valibotResolver(SummarizeLongDocFormSchema),
     defaultValues: {
-      apiToken: '',
+      apiToken,
       documentText: '',
+      modelName: 'gpt-4o',
     },
   });
 
   const onSubmit = async (data: SummarizeLongDocFormSchema) => {
     console.debug('[onSubmit.form]', form);
     console.debug('[onSubmit]', data);
+    setApiToken(data.apiToken);
   };
 
   return (
     <section>
       <Heading2>긴 문서 요약하기</Heading2>
+
+      <div className="mb-2">
+        <a
+          href="https://cookbook.openai.com/examples/summarizing_long_documents"
+          target="_blank"
+          rel="noreferrer noopener"
+          className="flex items-center gap-1 text-neutral-500 text-sm hover:underline"
+        >
+          <SquareArrowOutUpRightIcon size={16} />
+          <p>OpenAI Cookbook - Summarizing Long Documents</p>
+        </a>
+      </div>
 
       <Form {...form}>
         <form
@@ -66,7 +102,11 @@ export default function SummarizingLongDocumentsPage() {
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
-                      <Input placeholder="API Token" {...field} />
+                      <Input
+                        placeholder="API Token"
+                        type="password"
+                        {...field}
+                      />
                     </FormControl>
                   </FormItem>
                 )}
@@ -106,7 +146,62 @@ export default function SummarizingLongDocumentsPage() {
               <CardDescription>Tiktoken 으로 문서 토크나이징</CardDescription>
             </CardHeader>
 
-            <CardContent>{/*  */}</CardContent>
+            <CardContent>
+              <FormField
+                control={form.control}
+                name="modelName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="OpenAI 모델 선택" />
+                        </SelectTrigger>
+
+                        <SelectContent>
+                          <SelectItem value="gpt-4o">GPT-4o</SelectItem>
+                          <SelectItem value={'gpt-4o-mini' as TiktokenModel}>
+                            GPT-4o-mini
+                          </SelectItem>
+                          <SelectItem value="gpt-4">GPT-4</SelectItem>
+                          <SelectItem value={'gpt-3.5-turbo' as TiktokenModel}>
+                            GPT-3.5-Turbo
+                          </SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <button
+                type="button"
+                onClick={() => {
+                  fetcher
+                    .post('/api/v1/encoded-token', {
+                      json: {
+                        text: form.getValues().documentText,
+                        modelName: form.getValues().modelName,
+                      },
+                    })
+                    .json<{ tokens: Record<number, number> }>()
+                    .then((res) => {
+                      console.debug('[encodedTokens]', res);
+                      res?.tokens && setEncodedTokens(res.tokens);
+                    });
+                }}
+                disabled={form.getValues().documentText.length === 0}
+              >
+                토큰 갯수 불러오기
+              </button>
+
+              <div>
+                <p>토큰 갯수: {Object.keys(encodedTokens).length}</p>
+              </div>
+            </CardContent>
           </Card>
         </form>
       </Form>
