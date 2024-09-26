a5fad208844e1fcfd5780598d21a7aa87c8746f3
gitgitWi <gitgitwi@gmail.com>
2024-09-26 11:33:12 +0900

    feat(layouts/root) add footer with radix github icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
 import { GeistMono } from 'geist/font/mono';
 import type { Metadata } from 'next';
 import type { PropsWithChildren } from 'react';
@@ -13,10 +14,21 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="ko" className={cn(GeistMono.variable, 'antialiased')}>
-      <body className="p-4 bg-neutral-100">
-        <div className="flex flex-col max-w-2xl p-4 mx-auto bg-white border rounded-lg border-3 border-neutral-200">
+      <body className="bg-neutral-100 pt-4 h-full">
+        <div className="flex flex-col border-3 border-neutral-200 bg-white mx-auto p-4 border rounded-lg max-w-2xl">
           {children}
         </div>
+
+        <footer className="flex justify-start items-center gap-1 border-neutral-100 bg-white mx-auto mt-2 px-2 pt-2 border rounded-b-none rounded-lg max-w-2xl">
+          <a
+            href="https://github.com/gitgitWi/llm-tutorials-step-by-step"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="flex items-center gap-1 text-neutral-500 text-sm hover:underline"
+          >
+            <GitHubLogoIcon />
+          </a>
+        </footer>
       </body>
     </html>
   );
