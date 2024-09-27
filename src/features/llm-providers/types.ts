489d3cd07419ec6ff104be6f16e7f61659bf1c82
gitgitWi <gitgitwi@gmail.com>
2024-09-27 17:13:50 +0900

    feat(llm-providers) add providers enum and constants

---
+++ src/features/llm-providers/types.ts
@@ -0,0 +1,50 @@
+export enum LlmProviders {
+  AZURE_OPENAI = 'azure-openai',
+  OPENAI = 'openai',
+  GROQ = 'groq',
+  GOOGLE_AI = 'google-ai',
+  ANTHROPIC = 'anthropic',
+}
+
+export const LLM_PROVIDERS = [
+  {
+    groupName: 'OpenAI Compatible',
+    providers: [
+      {
+        key: LlmProviders.OPENAI,
+        name: 'OpenAI',
+        enabled: false,
+      },
+      {
+        key: LlmProviders.AZURE_OPENAI,
+        name: 'Azure OpenAI',
+        enabled: true,
+      },
+      {
+        key: LlmProviders.GROQ,
+        name: 'Groq',
+        enabled: true,
+      },
+    ],
+  },
+  {
+    groupName: 'Google AI',
+    providers: [
+      {
+        key: LlmProviders.GOOGLE_AI,
+        name: 'Google AI',
+        enabled: false,
+      },
+    ],
+  },
+  {
+    groupName: 'Anthropic',
+    providers: [
+      {
+        key: LlmProviders.ANTHROPIC,
+        name: 'Anthropic',
+        enabled: false,
+      },
+    ],
+  },
+] as const;
