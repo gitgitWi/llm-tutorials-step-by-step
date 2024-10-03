432482db195956e31f15cc70a2d446f22fe1f738
gitgitWi <gitgitwi@gmail.com>
2024-10-03 20:40:38 +0900

    feat(completion) set fetch timeout 60s

--- src/app/tutorials/summarizing-long-documents/page.tsx
+++ src/app/tutorials/summarizing-long-documents/page.tsx
@@ -107,6 +107,7 @@ export default function SummarizingLongDocumentsPage() {
           documentText: data.documentText,
           delimiter: data.delimiter,
         },
+        timeout: 60_000,
       })
       .json<SummarizeResponse>()
       .then((response) => {
